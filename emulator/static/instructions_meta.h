af7b3be1f13269b29a1e183e0e31742774f59c51
kaup <kalebroyolson+github@gmail.com>
2026-03-09 23:19:17 -0500

    More instruction metadata

--- emulator/static/instructions_meta.h
+++ emulator/static/instructions_meta.h
@@ -4,25 +4,32 @@
 #include <stdint.h>
 
 typedef enum {
-  IST_NOP = 0, // NOOP
-  IST_ADC, // Add value plus carry flag to A
-  IST_ADD // Add value
-} instruction_type_enum;
+  OP_NOP = 0, // NOOP
+  OP_ADC, // Add value plus carry flag to A
+  OP_ADD, // Add value
+  OP_LD, // Load
+  OP_LDI, // Load and increment
+  OP_LDD, // Load and decrement
+  OP_INC, // Increment
+  OP_DEC, // Decrement
+} op_type_enum;
 
 typedef enum {
-  IAT_NONE = 0, // default for args not defined/used
-  IAT_R8, // 8-bit cpu register
-  IAT_R16, // 16-bit cpu register
-  IAT_N8, // 8-bit immediate value
-  IAT_N16, // 16-bit immediate value
-  IAT_E8, // 8-bit signed offset
-  IAT_U3, // 3-bit unsigned bit index
-  IAT_CC, // condition code (Z, NZ, C, NC)
-  IAT_VEC // an RST vector (0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38)
-} instruction_arg_type_enum;
+  ARG_NONE = 0, // default for args not defined/used
+  ARG_R8, // 8-bit cpu register
+  ARG_R8_DREF, // 8-bit cpu register as mem address
+  ARG_R16, // 16-bit cpu register
+  ARG_R16_DREF, // 16-bit cpu register as mem address
+  ARG_N8, // 8-bit immediate value
+  ARG_N16, // 16-bit immediate value
+  ARG_A8, // 8-bit mem address (based on 0xFF00)
+  ARG_A16, // 16-bit mem address
+  ARG_E8, // 8-bit signed offset
+  ARG_U3, // 3-bit unsigned bit index
+  ARG_CC, // condition code (Z, NZ, C, NC)
+  ARG_VEC // an RST vector (0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38)
+} arg_type_enum;
 
-// Not actually sure if we will be using these enums
-// just creating an ordered map of numbers to values
 typedef enum {
   R8_PARSE = 0, // get the value from the byte(s) passed to the instruction
   R8_A,
@@ -34,6 +41,9 @@ typedef enum {
 
 typedef enum {
   R16_PARSE = 0, // get the value from the byte(s) passed to the instruction
+  R16_AF,
+  R16_BC,
+  R16_DE,
   R16_HL,
   R16_SP,
   R16_PC
@@ -49,9 +59,10 @@ typedef enum {
 typedef struct {
   uint8_t bytes; // including opcode byte
   uint8_t cycles; // T-Cycles (NOT M-cycles)
-  instruction_arg_type_enum arg1_type;
+  op_type_enum op; // Instruction type
+  arg_type_enum arg1_type;
   uint8_t arg1_value; // Only present if defined statically in the instruction
-  instruction_arg_type_enum arg2_type;
+  arg_type_enum arg2_type;
   uint8_t arg2_value; // Only present if defined statically in the instruction
 } instruction_meta_t;
 
